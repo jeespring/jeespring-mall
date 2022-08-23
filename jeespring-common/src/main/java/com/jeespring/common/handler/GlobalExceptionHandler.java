18f37c8ea6147563fe073a8357960f150e611606
张朝阳 <jeespring@163.com>
2022-08-24 00:12:23 +0800

    jeespring-common 初始化

---
+++ jeespring-common/src/main/java/com/jeespring/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.jeespring.common.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * GlobalExceptionHandler
+ *
+ * @author 张朝阳
+ * @date 2022/8/9 22:19:19
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
