73dfe1a5a269d5ef0a22972ee37a9801b5ce1054
张朝阳 <jeespring@163.com>
2022-08-28 20:32:25 +0800

    jeespring-user 搭建

---
+++ jeespring-user/src/test/java/com/jeespring/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
