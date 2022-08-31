d1ca7a4bf59a93651ffda9495d0f1341e799ab1d
张朝阳 <jeespring@163.com>
2022-08-31 23:04:43 +0800

    jeespring-search 搭建

---
+++ jeespring-search/src/test/java/com/jeespring/search/JeespringSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeespringSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
