b1a6ab658923c0bf185cecd6eaf2f567bc2183c2
张朝阳 <jeespring@163.com>
2022-09-06 23:58:40 +0800

    系统业务异常

---
+++ jeespring-common/src/main/java/com/jeespring/common/exception/BusinessException.java
@@ -0,0 +1,18 @@
+package com.jeespring.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * BusinessException
+ *
+ * @author 张朝阳
+ * @date 2022/9/3 15:01:01
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BusinessException extends RuntimeException{
+
+}
