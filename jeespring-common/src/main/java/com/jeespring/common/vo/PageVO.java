b4e79cbb52e4263af3e31277961e8e5c6e431d67
张朝阳 <jeespring@163.com>
2022-10-17 22:16:53 +0800

    jeespring-common 返回实体

--- jeespring-common/src/main/java/com/jeespring/common/vo/PageVO.java
+++ jeespring-common/src/main/java/com/jeespring/common/vo/PageVO.java
@@ -35,4 +35,46 @@ public class PageVO<T> implements Serializable {
      * 分页查询结果
      */
     private List<T> rows;
+
+    public long getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(long pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public long getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(long pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public long getPages() {
+        return pages;
+    }
+
+    public void setPages(long pages) {
+        this.pages = pages;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+
+
 }
