4f59b9cc4408ec0f57e9c03e09f0fc68cf9dee66
张朝阳 <jeespring@163.com>
2023-02-03 00:00:14 +0800

    jeespring-common 异常

--- jeespring-common/src/main/java/com/jeespring/common/exception/BusinessException.java
+++ jeespring-common/src/main/java/com/jeespring/common/exception/BusinessException.java
@@ -4,8 +4,43 @@ package com.jeespring.common.exception;
  * BusinessException
  *
  * @author 张朝阳
- * @date 2022/9/3 15:01:01
+ * @date 2022/12/3 15:01:01
  */
-public class BusinessException extends RuntimeException{
+public class BusinessException extends RuntimeException {
 
+    /**
+     * 错误码
+     */
+    private String code;
+
+    /**
+     * 错误信息
+     */
+    private String message;
+
+    public BusinessException() {
+    }
+
+    public BusinessException(String code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
