fd1b38182b0454a5d5f2c9e9cf25afd9b793a8fc
张朝阳 <jeespring@163.com>
2023-02-02 23:45:42 +0800

    jeespring-common 异常

--- jeespring-common/src/main/java/com/jeespring/common/exception/PermissionException.java
+++ jeespring-common/src/main/java/com/jeespring/common/exception/PermissionException.java
@@ -6,5 +6,6 @@ package com.jeespring.common.exception;
  * @author 张朝阳
  * @date 2022/10/11 23:21:21
  */
-public class PermissionException {
+public class PermissionException extends RuntimeException{
+
 }
---
+++ jeespring-common/src/main/java/com/jeespring/common/exception/RedisException.java
@@ -0,0 +1,11 @@
+package com.jeespring.common.exception;
+
+/**
+ * RedisException
+ *
+ * @author 张朝阳
+ * @date 2023/2/2 23:43:43
+ */
+public class RedisException extends RuntimeException{
+
+}
