b75fbb8e93d779e74e6ea04e8264022cd574453d
张朝阳 <jeespring@163.com>
2023-02-04 23:02:32 +0800

    jeespring-common 异常

---
+++ jeespring-common/src/main/java/com/jeespring/common/exception/RedisLockException.java
@@ -0,0 +1,11 @@
+package com.jeespring.common.exception;
+
+/**
+ * RedisLockException
+ *
+ * @author 张朝阳
+ * @date 2023/2/4 23:02:02
+ */
+public class RedisLockException extends RuntimeException{
+
+}
